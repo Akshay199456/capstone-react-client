29a03fa53d5c7ccbd0f5c2059c154ea86257b82d
Akshay199456 <akshay.kum94@gmail.com>
2019-03-08 10:23:04 -0800

    Made changes

--- capstone-react-client/src/containers/ResultContainer/index.js
+++ capstone-react-client/src/containers/ResultContainer/index.js
@@ -18,28 +18,37 @@ class ResultContainer extends Component{
 
 
 	fetchYouTubeResults = async () => {
-		const response = await fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q='+this.state.queryString+'&key=AIzaSyBCCWqoHDi9zuS6M-i8yXxvkZ8esqp3bX8',{
+		const response = await fetch('http://localhost:9000/api/v1/result/youtube/' + this.state.queryString,{
 			credentials: 'include'
 		});
 
-		console.log("Response from YouTube: ", response);
+		console.log("Response from youtube: ", response);
 
-		if(response.ok){
-			const parsedResponse = await response.json();
-			console.log("ParsedResponse from Youtube: ", parsedResponse);
+		const parsedResponse = await response.json();
+		console.log("ParsedResponse from Youtube: ", parsedResponse);
 
-			const videoIdArray = [];
-			const videos = parsedResponse.items;
-			for (let i=0; i<videos.length; i++){
-				console.log("Video ID: ", videos[i].id.videoId);
-				const embedString = 'https://www.youtube.com/embed/'+videos[i].id.videoId;
-				videoIdArray.push(embedString);
-			}
+		// const response = await fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q='+this.state.queryString+,{
+		// 	credentials: 'include'
+		// });
 
-			this.setState({
-				youTubeVideos: videoIdArray
-			})
-		}
+		// console.log("Response from YouTube: ", response);
+
+		// if(response.ok){
+		// 	const parsedResponse = await response.json();
+		// 	console.log("ParsedResponse from Youtube: ", parsedResponse);
+
+		// 	const videoIdArray = [];
+		// 	const videos = parsedResponse.items;
+		// 	for (let i=0; i<videos.length; i++){
+		// 		console.log("Video ID: ", videos[i].id.videoId);
+		// 		const embedString = 'https://www.youtube.com/embed/'+videos[i].id.videoId;
+		// 		videoIdArray.push(embedString);
+		// 	}
+
+		// 	this.setState({
+		// 		youTubeVideos: videoIdArray
+		// 	});
+		// }
 	}
 
 	render(){
