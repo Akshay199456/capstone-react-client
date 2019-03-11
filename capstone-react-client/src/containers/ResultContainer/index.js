9468ed483056124f3aaf0401d6269b28b0a7cf68
Akshay199456 <akshay.kum94@gmail.com>
2019-03-10 23:00:51 -0700

    Added pie chart

--- capstone-react-client/src/containers/ResultContainer/index.js
+++ capstone-react-client/src/containers/ResultContainer/index.js
@@ -8,11 +8,15 @@ import Tumblr from '../../components/Tumblr';
 import YouTubeMusic from '../../components/YouTubeMusic';
 import LastFm from '../../components/LastFm';
 
+import {RadialChart} from 'react-vis';
+
+
 class ResultContainer extends Component{
 	constructor(){
 		super();
 		
 		this.state ={
+			// Data from different platforms
 			queryString: localStorage.getItem('queryString'),
 			youTubeVideos: [],
 			youTubeVideosCount: 0,
@@ -27,19 +31,30 @@ class ResultContainer extends Component{
 			youTubeMusicVideos: [],
 			youTubeMusicVideosCount: 0,
 			lastFmResults: [],
-			lastFmResultsCount: 0
+			lastFmResultsCount: 0,
+
+			// Data to display radial chart
+			dataReadyToDisplay: false,
+			entertainmentCount: 0,
+			musicCount: 0,
+			newsCount: 0,
+			entertainmentPercentage: 0,
+			musicPercentage: 0,
+			newsPercentage: 0 
 
 		}
 	}
 
-	componentDidMount(){
-		// this.fetchYouTubeResults();
-		// this.fetchNYTNewsResults();
-		// this.fetchNewsAPIResults();
-		// this.fetchTechCrunchResults();
-		this.fetchTumblrResults();
-		// this.fetchYouTubeMusicResults();
-		// this.fetchLastFmResults();
+	componentDidMount = async () => {
+		await this.fetchYouTubeResults();
+		await this.fetchNYTNewsResults();
+		await this.fetchNewsAPIResults();
+		await this.fetchTechCrunchResults();
+		await this.fetchTumblrResults();
+		await this.fetchYouTubeMusicResults();
+		await this.fetchLastFmResults();
+
+		await this.generateStatsOverall();
 	}
 
 
@@ -288,13 +303,49 @@ class ResultContainer extends Component{
 				lastFmResultsCount: parseInt((parsedResponse.data.results)['opensearch:totalResults'], 10)
 			});
 		}
-
 	}
 
 
+	generateStatsOverall = () =>{
+		console.log("We have triggered generateStatsOverall");
 
+		const entertainmentCount = this.state.youTubeVideosCount + this.state.tumblrDataCount;
+		const musicCount = this.state.youTubeMusicVideosCount + this.state.lastFmResultsCount;
+		const newsCount = this.state.nytArticlesCount + this.state.newsAPIArticlesCount + this.state.techCrunchArticlesCount;
+
+		console.log("Entertainment Category Count: ", entertainmentCount);
+		console.log("Music Category Count: ", musicCount);
+		console.log("News Category Count: ", newsCount);
+
+		this.setState({
+			entertainmentCount: entertainmentCount,
+			musicCount: musicCount,
+			newsCount: newsCount
+		});
+
+		this.generateDegreeEquivalent(entertainmentCount, musicCount, newsCount);
+	}
+
+	generateDegreeEquivalent = (category1Data, category2Data, category3Data) =>{
+		const total = category1Data + category2Data + category3Data;
+		const degree1 = Math.round((category1Data/total)*360);
+		const degree2 = Math.round((category2Data/total)*360);
+		const degree3 = Math.round((category3Data/total)*360);
+
+		console.log("Entertainment Category Percentage: ", degree1);
+		console.log("Music Category Percentage: ", degree2);
+		console.log("News Category Percentage: ", degree3);
+
+		this.setState({
+			entertainmentPercentage: degree1,
+			musicPercentage: degree2,
+			newsPercentage: degree3,
+			dataReadyToDisplay: true
+		});
+	}
 
 	render(){
+
 		console.log("State from Result Container: ", this.state);
 		return(
 			<div>
@@ -307,6 +358,16 @@ class ResultContainer extends Component{
 				{ this.state.tumblrData.length === 0 ? null : <Tumblr tumblrData={this.state.tumblrData}/>}
 				{ this.state.youTubeMusicVideos.length === 0 ? null : <YouTubeMusic youTubeMusicVideos={this.state.youTubeMusicVideos}/>}
 				{ this.state.lastFmResults.length === 0 ? null : <LastFm lastFmResults={this.state.lastFmResults}/>}
+
+				{ this.state.dataReadyToDisplay ?
+					<RadialChart
+						data={[{angle: this.state.entertainmentPercentage, color: 0.3, label: 'Entertainment', sublabel:'Percentage:'+this.state.entertainmentPercentage}, {angle: this.state.musicPercentage, color: 1.6, label: 'Music'}, {angle: this.state.newsPercentage, color: 1.9, label: 'News'}]}
+						width={300}
+						height={300} 
+						showLabels={true}
+					/>
+					 : null
+				}
 			</div>
 		);
 	}
